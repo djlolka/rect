c0de87ae1782003f0e354996899490dd855ed124
djlolka <nekrasovneketa68@gmail.com>
2021-11-26 14:07:29 +0500

    pygame

--- game.py
+++ game.py
@@ -11,23 +11,15 @@ def draw(screen):
 
 
 if __name__ == '__main__':
-    # инициализация Pygame:
     pygame.init()
     pygame.display.set_caption('Крест')
-    # размеры окна:
     if a.isdigit() and b.isdigit():
         size = width, height = int(a), int(b)
         screen = pygame.display.set_mode(size)
-        # формирование кадра:
-        # команды рисования на холсте
         draw(screen)
-        # ...
-        # смена (отрисовка) кадра:
         pygame.display.flip()
-        # ожидание закрытия окна:
         while pygame.event.wait().type != pygame.QUIT:
             pass
-        # завершение работы:
         pygame.quit()
     else:
         print('Неправильный формат ввода')
